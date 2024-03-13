ac16aef58269bd0c5dad3343a4e20632af5200ff
samucarafi <160398348+samucarafi@users.noreply.github.com>
2024-03-13 09:01:41 -0300

    javaScript

---
+++ Aula 04/ex008.js
@@ -0,0 +1,8 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if (vel > 60){ //CONDIÇÃO SIMPLES
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+} 
+    console.log ('Dirija sempre usando cindo de segurança')
+
+
