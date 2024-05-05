e1f27670c248990fab31b773c9b05e43f875d75c
samucarafi <160398348+samucarafi@users.noreply.github.com>
2024-05-05 20:32:47 +0000

    javascript

---
+++ CFB-Cursos/Aula05-Operadores-Relacionais/Aula05.js
@@ -0,0 +1,11 @@
+//Operadores Relacionais: > >= < <= == !=
+
+let num1=10
+let num2=5
+let num3=10
+
+console.log(!(num1==num3)) // ! é uma negação da expressão entre parenteses.
+
+console.log(num1 != num3) // != diferente
+
+// === é mais profundo que ==, pois leva em consideração se ambos são numbers ou string.
---
+++ CFB-Cursos/Aula06-Operadores-Lógicos/Aula06.js
@@ -0,0 +1,4 @@
+/* && -> and; || -> or; ! -> not*/
+let n1 = 2, n2 = 5, n3 = 7, n4 = 10;
+console.log((n1 < n2) && (n3 < n4))
+console.log((n1 > n2) || (n2 < n3))
