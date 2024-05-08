c97e24a2e531d488b980a22265300101b946f862
samucarafi <160398348+samucarafi@users.noreply.github.com>
2024-05-08 11:53:11 +0000

    CFB cursos

---
+++ CFB-Cursos/Aula09-Operadores-Ternários/Aula09.js
@@ -0,0 +1,11 @@
+let num=15
+let num1=5
+
+
+let res=(!(num%2) ? "Par": "Impar")// ternário para dizer que o número é impar ou par, o resultado de num%2=0 significa que é par logo será quando o resultado for false=0 quando for par num%2=1 logo daria verdadeiro, para colocar o par como verdadeiro e impar como falso foi utilizado ! negação de num%2
+
+let res1=(num > num1) ? "Verdadeiro":"Falso"//ternário
+
+console.log(res)
+
+console.log(res1)
