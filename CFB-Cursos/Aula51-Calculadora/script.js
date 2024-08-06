0c46fd1c7e2937ca3c53d68c5e75ce229489acbd
samucarafi <160398348+samucarafi@users.noreply.github.com>
2024-08-06 12:38:49 +0000

    calc

--- CFB-Cursos/Aula51-Calculadora/script.js
+++ CFB-Cursos/Aula51-Calculadora/script.js
@@ -5,6 +5,10 @@ const display=document.querySelector('.display')
 const ton=document.getElementById('ton')
 const tlimpar=document.getElementById('tlimpar')
 const tigual=document.getElementById('tigual')
+const tcpy=document.getElementById('tcpy')
+const calc=document.getElementById('calc')
+const calc_aba=document.getElementById('calc_aba')
+const seta=document.getElementById('seta')
 
 let sinal=false//controla para não permitir dois sinais consecutivos, ao apertar um sinal irá para true e ao apertar limpar ou um numero volta a false
 
@@ -59,4 +63,21 @@ tigual.addEventListener('click',(evt)=>{
     decimal=false
     const res=eval(display.innerHTML)//eval calcula automaticamente através da impressão do display seu calculo.
     display.innerHTML=res
+})
+
+tcpy.addEventListener('click',(evt)=>{
+    navigator.clipboard.writeText(display.innerHTML)//comando para copiar texto do display para area de transferencia e conseguir colar em outro lugar ou app
+    //No caso de estar copiando algo de algum imput e para mobile utilizar a forma a seguir:
+    //teste.select()//apenas mobile(selecionar texto do imput)
+    //teste.setSelectionRange(0,999999)//apenas mobile(determinar intervalo de seleção,colocar valor alto)
+    //navigator.clipboard.whiteText(text.value)
+})
+
+calc_aba.addEventListener('click',(evt)=>{
+    calc.classList.toggle('calc_exibir')//ao clicar na aba colocar a class calc_exibir se ela não tiver atribuida ao calc ou tirar caso contrário
+    if(calc.classList.contains("calc_exibir")){//verifica se está aberto a aba
+        seta.innerHTML='arrow_left'//se sim seta para esquerda
+    }else{
+        seta.innerHTML='arrow_right'//se não seta para direita
+    }
 })
